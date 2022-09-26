e2e542178965849b11ba7da986c71bfffe950fc4
horvath <patricia.horvath@tum.de>
2022-09-26 16:53:31 +0200

    changed name

---
+++ simple_mqtt/node2/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+hn=$(pos_get_variable hostname)
+a_global_variable=$(pos_get_variable a/global/variable --from-global)
+a_local_variable=$(pos_get_variable a/local/variable)
+
+echo "Setting up node2..."
+echo "hostname is $hn according to pos"
+
+# clone git repo
+repository="https://github.com/patriciaaviv/mosquitto.git"
+# which folder is mine on the test node?
+mkdir mqtt
+localFolder="/root/mqtt/"
+git clone "$repository" "$localFolder"
+
+# compile the files
+cd /root/mqtt/mosquitto
+make
+
+echo "setup of client node completed"
+
+## add iptables rule here?
