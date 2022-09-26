9783047e4536fcefed73e83043d1ac6e166b9d60
horvath <patricia.horvath@tum.de>
2022-09-27 00:20:20 +0200

    added interface config for client

--- simple_mqtt/node2/setup.sh
+++ simple_mqtt/node2/setup.sh
@@ -18,10 +18,12 @@ mkdir mqtt
 localFolder="/root/mqtt/mosquitto"
 git clone "$repository" "$localFolder"
 
-# compile the files
-#cd /mqtt/mosquitto
-#make
+# set up interfaces
+$IFACE1="eno1"
+ip link set dev $IFACE1 up
+
+# include iptables rule to randomize IP address
+iptables -t nat -A POSTROUTING -o $IFACE1 -p tcp -m tcp --syn -m statistic --mode random --probability 0.5 -j SNAT --to-source 172.16.1.1
+#iptables -t nat -A POSTROUTING -o $IFACE1 -m statistic --mode nth --every 1 --packet 0 -j SNAT --to-source 172.16.1.1
 
 echo "setup of client node completed"
-
-## add iptables rule here?
