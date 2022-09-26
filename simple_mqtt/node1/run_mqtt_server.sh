d4eb0b3ad804605b98508e6a50729e1f5b5a7575
horvath <patricia.horvath@tum.de>
2022-09-26 22:39:22 +0200

    changed path

--- simple_mqtt/node1/run_mqtt_server.sh
+++ simple_mqtt/node1/run_mqtt_server.sh
@@ -3,7 +3,7 @@
 echo "Starting the mosquitto server now ..."
 # cd into where my repo is
 #echo "current dir is $PWD", current dir is /root
-DIR= "$(pwd)/mosquitto/src"
+DIR= "$(pwd)/mqtt/mosquitto/src"
 cd $DIR
 make 
 ./mosquitto -v
