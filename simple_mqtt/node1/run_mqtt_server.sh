f59aa27510a97f008d912132f4177d7f2ac01c8f
horvath <patricia.horvath@tum.de>
2022-09-27 14:14:00 +0200

    changed path

--- simple_mqtt/node1/run_mqtt_server.sh
+++ simple_mqtt/node1/run_mqtt_server.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-local="/root/mqtt"
+#local="/root/mqtt"
 # exit on error
 set -e
 # log every command
 set -x
 
-echo "Starting the mosquitto server now ..."
+# clone git repo
+repository="https://github.com/patriciaaviv/mosquitto.git"
+# which folder is mine on the test node?
+mkdir mqtt
+localFolder="/root/mqtt/mosquitto"
+git clone "$repository" "$localFolder"
 
+echo "Starting the mosquitto server now ..."
 
 # cd into where my repo is
 #echo "current dir is $PWD", current dir is /root
 #DIR= "$(pwd)/mqtt/mosquitto/src"
 #cd $DIR
-cd $local/mosquitto/src/
+cd mqtt/mosquitto/src/
 ls -la #1>&2
 make 
 ./mosquitto -v
--- simple_mqtt/node1/setup.sh
+++ simple_mqtt/node1/setup.sh
@@ -13,12 +13,6 @@ apt-get update -y
 apt-get install tshark
 apt-get install libssl-dev
 
-# clone git repo
-repository="https://github.com/patriciaaviv/mosquitto.git"
-# which folder is mine on the test node?
-mkdir mqtt
-localFolder="/root/mqtt/mosquitto"
-git clone "$repository" "$localFolder"
 
 echo "setup of server node completed"
 
