5146b5df27ab0cc430e659193a607126be500ada
horvath <patricia.horvath@tum.de>
2022-09-27 16:22:28 +0200

    added conf file path

--- simple_mqtt/node1/run_mqtt_server.sh
+++ simple_mqtt/node1/run_mqtt_server.sh
@@ -21,11 +21,12 @@ DEBIAN_FRONTEND=noninteractive apt-get install libssl-dev -y
 DEBIAN_FRONTEND=noninteractive apt-get install mosquitto -y
 
 # configure mosquitto broker to allow remote connections
-echo "listener 1883 0.0.0.0 \nallow_anonymous true" >> /etc/mosquitto/mosquitto.conf
+echo "listener 1883 0.0.0.0" >> /etc/mosquitto/mosquitto.conf
+echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
 
 echo "Starting the mosquitto server now ..."
 # write server output into a txt file
-mosquitto -p 1883 -v 2>&1 | tee -a /root/mqtt_server_output.txt
+mosquitto -p 1883 -v -c /etc/mosquitto/mosquitto.conf >> /root/mqtt_server_output.txt
 
 # cd into where my repo is
 # cd mqtt/mosquitto/src/
