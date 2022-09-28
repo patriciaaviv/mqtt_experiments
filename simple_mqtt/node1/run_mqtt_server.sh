278937de6714e50f8db29aa66447683fb658298b
horvath <patricia.horvath@tum.de>
2022-09-28 23:10:35 +0200

    changed client setup

--- simple_mqtt/node1/run_mqtt_server.sh
+++ simple_mqtt/node1/run_mqtt_server.sh
@@ -7,27 +7,6 @@ set -e
 # log every command
 set -x
 
-# apt-get update -y
-
-# DEBIAN_FRONTEND=noninteractive apt-get -y install tshark linux-perf libssl-dev nmap telnet net-tools ufw build-essential gcc make cmake git
-
-# mkdir mqtt
-# cd mqtt
-# git clone https://github.com/eclipse/mosquitto.git
-# cd mosquitto/src
-# make WITH_DOCS=no
-# # cp src/mosquitto ../..
-
-
-# # mosquitto user
-# adduser --disabled-password --gecos "" mosquitto
-
-#echo "setup of server node completed"
-
-# ufw allow 22
-# ufw allow 1883 
-# ufw enable
-
 # configure mosquitto broker to allow remote connections
 echo "listener 1883 172.16.2.1" >> /root/mqtt/mosquitto/mosquitto.conf
 echo "allow_anonymous true" >> /root/mqtt/mosquitto/mosquitto.conf
--- simple_mqtt/node2/run_mqtt_sub.sh
+++ simple_mqtt/node2/run_mqtt_sub.sh
@@ -5,38 +5,38 @@ set -e
 # log every command
 set -x
 
-echo "Setting up node2..."
+#echo "Setting up node2..."
 
-# update apt-get 
-apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get -y install tshark libssl-dev linux-perf tshark build-essential gcc make cmake git iptab
+# # update apt-get 
+# apt-get update -y
+# DEBIAN_FRONTEND=noninteractive apt-get -y install tshark libssl-dev linux-perf tshark build-essential gcc make cmake git iptab
 
-# clone mosquitto git repo
-git clone https://github.com/eclipse/mosquitto.git
-cd mosquitto
-mkdir build
-cd build
+# # clone mosquitto git repo
+# git clone https://github.com/eclipse/mosquitto.git
+# cd mosquitto
+# mkdir build
+# cd build
 
-#cmake and make invocation: build static libraries
-cmake -DWITH_STATIC_LIBRARIES=ON ..
-make
+# #cmake and make invocation: build static libraries
+# cmake -DWITH_STATIC_LIBRARIES=ON ..
+# make
 
-#copy resulting library in the dir you were called from
-cp lib/libmosquitto_static.a ../../libmosquitto.a
+# #copy resulting library in the dir you were called from
+# cp lib/libmosquitto_static.a ../../libmosquitto.a
 
-#make a "headers" folder in the dir you were called from and copy header files there
-cd ../..
-mkdir headers
-cp mosquitto/lib/*.h headers/
+# #make a "headers" folder in the dir you were called from and copy header files there
+# cd ../..
+# mkdir headers
+# cp mosquitto/lib/*.h headers/
 
-gcc mosquitto_publisher.c libmosquitto.a -Iheaders -lcrypto -lssl -lpthread -o publisher
-gcc mosquitto_subscriber.c libmosquitto.a -Iheaders -lcrypto -lssl -lpthread -o subscriber
+# gcc mosquitto_publisher.c libmosquitto.a -Iheaders -lcrypto -lssl -lpthread -o publisher
+# gcc mosquitto_subscriber.c libmosquitto.a -Iheaders -lcrypto -lssl -lpthread -o subscriber
 
-# set up interfaces
-$IFACE1="eno1"
-ip link set dev $IFACE1 up
+# # set up interfaces
+# $IFACE1="eno1"
+# ip link set dev $IFACE1 up
 
-echo "setup of client node completed"
+# echo "setup of client node completed"
 
 
 echo "Starting the mosquitto server now ..."
